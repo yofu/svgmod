07b430d9b14e30dabe31933980fa43e6061174a0
yofu <y.of.u.142857@gmail.com>
2015-11-25 12:09:14 +0900

    set s/*/*/ as no interpretation. add t/*/*/ for interpreing tex

--- svgmod/parse.go
+++ svgmod/parse.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	subpat = regexp.MustCompile("^s/([^/]+)/([^/]*)/?$")
+	subtexpat = regexp.MustCompile("^t/([^/]+)/([^/]*)/?$")
 	label  = regexp.MustCompile("^([xy]label) (.+)$")
 	title  = regexp.MustCompile("^(title[0-9]*) (.+)$")
 )
@@ -15,6 +16,9 @@ func Parse(txt, font string) (*Command, error) {
 	switch {
 	case subpat.MatchString(txt):
 		fs := subpat.FindStringSubmatch(txt)
+		return CommandSubstitute(fs[1], fs[2])
+	case subtexpat.MatchString(txt):
+		fs := subtexpat.FindStringSubmatch(txt)
 		return CommandSubstitute(fs[1], tex2svg(fs[2], font))
 	case label.MatchString(txt):
 		fs := label.FindStringSubmatch(txt)
